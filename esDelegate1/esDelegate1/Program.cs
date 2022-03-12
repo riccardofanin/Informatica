cc5500931e131610946f0a3a1ca0c15413f71d9f
rickycloud <93150146+rickycloud@users.noreply.github.com>
2022-03-12 11:33:38 +0100

    es delegate 1 e 2

---
+++ esDelegate1/esDelegate1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace esDelegate1
+{
+    class Program
+    {
+        static int[] vett = { -1, 2, 19, -2, 0, 4 };
+        public delegate void Del(int[] r);
+
+        static void Main(string[] args)
+        {
+            Del delegato = confronto;
+            delegato(vett);
+        }
+
+        private static void confronto(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.WriteLine("Il numero " + arr[i] + " è minore di 0");
+                }
+                else if (arr[i] > 0)
+                {
+                    Console.WriteLine("Il numero " + arr[i] + " è maggiore di 0");
+                }
+                else if (arr[i] == 0)
+                {
+                    Console.WriteLine("Il numero " + arr[i] + " è uguale di 0");
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ esDelegate2/esDelegate2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace esDelegate2
+{
+    class Program
+    {
+        public delegate double Del(double number);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire valore: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Del delegato = Square;
+            delegato += Cube;
+
+            double number = 0;
+            foreach (Del item in delegato.GetInvocationList())
+            {
+                if(item.Method.Name == "Square")
+                {
+                    number = item(n);
+                    Console.WriteLine("Il quadrato: " + number);
+                }
+                else if(item.Method.Name == "Cube")
+                {
+                    Console.WriteLine("Il cubo: " + item(number));
+                }
+            }
+        }
+
+        public static double Square(double n)
+        {         
+            return Math.Pow(n, 2);
+        }
+
+        public static double Cube(double n)
+        {
+            return Math.Pow(n, 3);
+        }
+    }
+}
